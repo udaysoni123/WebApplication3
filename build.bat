5e134ca7358b889b11e560b586cc61b5f9358b07
Uday Soni <45026149+udaysoni123@users.noreply.github.com>
2018-11-29 18:32:21 +0530

    build file
    added

---
+++ build.bat
@@ -0,0 +1,20 @@
+@echo Off
+set config=%1
+if "%config%" == "" (
+   set config=Release
+)
+ 
+set version=1.0.0
+if not "%PackageVersion%" == "" (
+   set version=%PackageVersion%
+)
+
+set nuget=
+if "%nuget%" == "" (
+	set nuget=nuget
+)
+
+
+mkdir Build
+
+%nuget% pack "WebApplication3/ClassLibrary1/bin/Debug/ClassLibrary1_data.1.0.0.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
