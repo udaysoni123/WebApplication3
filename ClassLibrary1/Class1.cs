1c84355f999c1275acf974f3e147c866d653f6c9
udaysoni123 <45026149+udaysoni123@users.noreply.github.com>
2018-11-29 15:35:40 +0530

    3
    3

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -7,5 +7,6 @@ namespace ClassLibrary1
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public int da { get; set; }
     }
 }
