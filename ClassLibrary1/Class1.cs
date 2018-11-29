bc3f15613b9955829697614ea912da9919f16139
udaysoni123 <45026149+udaysoni123@users.noreply.github.com>
2018-11-29 18:21:31 +0530

    7
    test

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -9,5 +9,6 @@ namespace ClassLibrary1
         public string Password { get; set; }
         public int da { get; set; }
         public int testno { get; set; }
+        public string testvalue { get; set; }
     }
 }
