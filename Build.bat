dee5f764bf0e930ca9f578e59e3bdf4212ab2019
Uday Soni <45026149+udaysoni123@users.noreply.github.com>
2018-11-30 15:28:32 +0530

    update
    build

--- Build.bat
+++ Build.bat
@@ -14,6 +14,6 @@ if "%nuget%" == "" (
 	set nuget=nuget
 )
 
-%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild WebApplication3.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
+%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild WebApplication3\WebApplication3.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
 
-%nuget% pack "ClassLibrary1/bin/Debug/ClassLibrary1_data.1.0.0.nupkg" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "WebApplication3\ClassLibrary1/bin/Debug/ClassLibrary1_data.1.0.0.nupkg" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
